7c4f6be39b5838c896ae53abef62afe3d85d3b5e
pedrosero <pedroangelserrano@hotmail.es>
2023-11-14 21:39:51 +0100

    Parte teoría Medrano subiendoArchivos

--- UD5/1_cookies.php
+++ UD5/1_cookies.php
@@ -29,6 +29,14 @@
         echo"<br><b>Test de cookies2: </b>". $_COOKIE["TestCookie2"];   
 
         print_r($_COOKIE);
+        /**
+                 * "TestCookie": Es el nombre de la cookie.
+                *$value: Es el valor de la cookie.
+                *time() - 3600: Es el tiempo de expiración de la cookie. En este caso, se establece en el tiempo actual (time()) menos 3600 segundos, lo que significa que la cookie expirará 3600 segundos (1 hora) antes del momento actual, es decir, se eliminará.
+                *"/rasmus/": Es el path o ruta para la cual la cookie está disponible. En este caso, la cookie solo está disponible para páginas bajo el path "/rasmus/" en el servidor.
+                *"example.com": Es el dominio para el cual la cookie está disponible. En este caso, la cookie solo está disponible para el dominio "example.com".
+                *1: Es un parámetro booleano (verdadero o falso) que indica si la cookie debe ser enviada solo a través de conexiones seguras (HTTPS). En este caso, se establece en 1, lo que significa verdadero. Esto indica que la cookie debe ser segura y solo se enviará a través de conexiones HTTPS.
+                */
     ?>
     
 </body>
--- UD5/subiendoArchivos.php
+++ UD5/subiendoArchivos.php
@@ -6,26 +6,31 @@
     <title>Document</title>
 </head>
 <body>
+
+    <!--enctype="multipart/form-data" para permitir la carga de archivos. 
+    La acción del formulario (action) está establecida en $_SERVER['PHP_SELF'], que hace referencia al script PHP actual.-->
         <form enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>"  method="POST">
                 Archivo: <input name="archivoEnviado" type="file" />
             <br />
                 <input type="submit" name="btnSubir" value="Subir" />
         </form>
     <?php
+        /*
+            Utiliza is_uploaded_file para verificar si se ha enviado un archivo. 
+            $_FILES['archivoEnviado']['tmp_name'] es el nombre temporal del archivo en el servidor.
+        */
+
         if (isset($_POST['btnSubir']) && $_POST['btnSubir'] == 'Subir') {
             if (is_uploaded_file($_FILES['archivoEnviado']['tmp_name'])) {
                 // subido con éxito
                 $nombre = $_FILES['archivoEnviado']['name'];
+                /*
+                    Si el archivo se ha cargado con éxito, se obtiene el nombre original del archivo ($nombre) y se 
+                    utiliza move_uploaded_file para mover el archivo a una ubicación específica en el servidor.
+                */
                 move_uploaded_file($_FILES['archivoEnviado']['tmp_name'], "/xampp2/htdocs/dwes-localhost/UD5/subidas{$nombre}");
                 echo "<p>Archivo $nombre subido con éxito</p>";
-                /**
-                 * "TestCookie": Es el nombre de la cookie.
-                *$value: Es el valor de la cookie.
-                *time() - 3600: Es el tiempo de expiración de la cookie. En este caso, se establece en el tiempo actual (time()) menos 3600 segundos, lo que significa que la cookie expirará 3600 segundos (1 hora) antes del momento actual, es decir, se eliminará.
-                *"/rasmus/": Es el path o ruta para la cual la cookie está disponible. En este caso, la cookie solo está disponible para páginas bajo el path "/rasmus/" en el servidor.
-                *"example.com": Es el dominio para el cual la cookie está disponible. En este caso, la cookie solo está disponible para el dominio "example.com".
-                *1: Es un parámetro booleano (verdadero o falso) que indica si la cookie debe ser enviada solo a través de conexiones seguras (HTTPS). En este caso, se establece en 1, lo que significa verdadero. Esto indica que la cookie debe ser segura y solo se enviará a través de conexiones HTTPS.
-                */
+                
             }
         }
     ?>
